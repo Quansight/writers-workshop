3244432666913f1804ca91702df8a582469aace7
tonyfast <tony.fast@gmail.com>
2021-05-05 17:20:40 -0400

    build pdf with conda

--- qriter/noxfile.py
+++ qriter/noxfile.py
@@ -11,3 +11,13 @@ CI = "GITHUB_ACTION" in os.environ or "READTHEDOCS" in os.environ
 def docs(session):
     session.install(*"""-rworks/requirements-docs.txt --ignore-installed""".split())
     session.run(*"""doit build_docs""".split())
+
+@nox.session(reuse_venv=True, python=False if CI else "3.8", venv_backend="conda")
+def pdf(session):
+    session.conda_install(
+        *"""jupyter-book[sphinx,pdflatex] texlive-core -cconda-forge""".split()
+    )
+    session.install("bindep")
+    session.run("bindep")
+    session.run(*"jb build . --toc qww/toc.yml --config qww/config.yml".split())
+    session.run(*"jb build . --toc qww/toc.yml --config qww/config.yml --builder pdflatex".split())
