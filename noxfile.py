00a2eb6892b0e72a7f60b17d616349d30ff12bd7
tonyfast <tony.fast@gmail.com>
2021-05-06 21:45:39 -0400

    start the binder

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,15 @@
 import nox
+import shutil
+
+if shutil.which("mamba"):
+    # speed things up with mamba if you have it
+    if "_run" not in locals():
+        _run = nox.sessions.Session._run
+
+    def run(self, *args, **kwargs):
+        if args[0] == "conda":
+            args = ("mamba",) + args[1:]
+        return _run(self, *args, **kwargs)
 
 @nox.session(python="3.8", reuse_venv=True, venv_backend="conda")
 def docs(session):
